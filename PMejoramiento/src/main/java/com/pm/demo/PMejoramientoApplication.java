279b939fc5faad9882b3ed6b3d128035de24f02c
Roman Bohorquez <romanorlandoboc@gmail.com>
2025-11-02 23:38:14 -0500

    Version_Inicial

---
+++ PMejoramiento/src/main/java/com/pm/demo/PMejoramientoApplication.java
@@ -0,0 +1,13 @@
+package com.pm.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PMejoramientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PMejoramientoApplication.class, args);
+	}
+
+}
